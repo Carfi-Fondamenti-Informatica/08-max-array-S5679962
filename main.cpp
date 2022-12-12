f95e019dd03fb14fc00c9d31207e3255b63c42ad
Gregorio Minetto <117943888+S5679962@users.noreply.github.com>
2022-12-12 21:34:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+
+    int i, a;
+    cin >> a;
+    float m[a], c;
+    for(i=0; i<a; i++){
+
+        cin >> m[i];
+    }
+    
+    c=max(m, a);
+    cout << c << endl;
+
 }
